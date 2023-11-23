654166f23259d05ff852d47df62e0ed43eb47e93
Paul Scanlon <pauliescanlon@gmail.com>
2023-11-23 14:42:33 -0500

    refactor: create cartman animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,26 +2,7 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {
-      fontFamily: {
-        MijaBold: ['MijaBold', 'sans-serif'],
-        ElenaRegular: ['ElenaRegular', 'sans-serif'],
-        ElenaBold: ['ElenaBold', 'sans-serif'],
-        ElenaItalic: ['ElenaItalic', 'sans-serif'],
-      },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            '*': {
-              fontFamily: 'ElenaRegular',
-            },
-            'h1, h2, h3, h4, h5, h6, a, button': {
-              fontFamily: 'MijaBold',
-            },
-          },
-        },
-      }),
-    },
+    extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
 };
